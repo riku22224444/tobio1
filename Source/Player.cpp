80cee8470046687c3af6d42c668e4941bb85770b
Maedaibuki <2240059@ecc.ac.jp>
2025-11-17 16:40:44 +0900

    とりあえず

--- Source/Player.cpp
+++ Source/Player.cpp
@@ -315,11 +315,6 @@ void Player::InputMove(float elapsedTime) {
 
 	float gripNow = lerp(grip, 3.0f, t);
 
-	//移動処理
-	Move(moveVec.x, moveVec.z, moveSpeed*4);
-	//旋回処理
-	Turn(elapsedTime, moveVec.x, moveVec.z, turnSpeed*2);
-
 }
 
 
